075c9453d6f3968da03b8c1c5139ff597114f5b9
fermin <fermin.hdez@gmail.com>
2025-02-13 23:17:49 +0100

    Update K6 performance test configuration with increased load stages

--- k6/api_test.js
+++ k6/api_test.js
@@ -3,14 +3,14 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '1m', target: 10 }, // Ramp-up to 10 users over 1 minute
-        { duration: '2m', target: 10 }, // Stay at 10 users for 2 minutes
-        { duration: '1m', target: 0 },  // Ramp-down to 0 users over 1 minute
+        { duration: '1m', target: 200 },
+        { duration: '2m', target: 300 },
+        { duration: '2m', target: 400 },
+        { duration: '1m', target: 0 },
     ],
 };
 
 export default function () {
-    sleep(5);
     // success
     let res = http.get('http://leaky-bucket:3010/');
     check(res, {
